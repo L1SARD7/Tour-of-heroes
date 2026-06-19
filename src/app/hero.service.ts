7c065702eb75eeb9808529989ad770e76a7f3c4f
Artem Klimchuk <arthemklimchuk7@gmail.com>
2026-06-19 08:49:02 +0300

    feat: add a form for creating a hero and update routing; enhance hero details and styles

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { MessageService } from './messages.service';
 import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
