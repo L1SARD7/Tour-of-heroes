7403f4c4d471c6913347c6fe2d2c1552b73e6f12
Artem Klimchuk <arthemklimchuk7@gmail.com>
2026-07-06 14:15:13 +0300

    feat: add dark interface and show actual date button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { throwIfEmpty } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
+  currentItem = 'Logo'
+  currentStyle = 'default';
+  anotherStyle = 'dark'
   title = 'Tour of heroes';
-}
+
+  onClick () {
+      console.log(this.currentStyle)
+    if (this.currentStyle === 'default') {
+      this.currentStyle = 'dark'
+      this.anotherStyle = 'default'
+
+    } 
+    else {
+      this.currentStyle = 'default'
+      this.anotherStyle = 'dark'
+    }
+
+      console.log(this.currentStyle)}
+
+  isShowActive = false
+  actualDate = 0
+  getDate() {
+    return Date.now()
+  }  
+      
+  showDate() {
+    console.log(this.actualDate, this.isShowActive)
+    this.actualDate = this.getDate()
+    this.isShowActive = true
+  }
+
+  log!: string
+  getLogs($event: string) {
+    console.log('fsdf')
+    this.log = $event
+  }
+
+} 
--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MessageService } from '../messages.service';
 
 @Component({
@@ -7,5 +7,11 @@ import { MessageService } from '../messages.service';
   styleUrls: ['./messages.component.less']
 })
 export class MessagesComponent {
+  @Input() item = ''
+  @Output() logEvent = new EventEmitter<string>()
+  logMessage() {
+    console.log('log')
+    this.logEvent.emit('log')
+  }
   constructor(public messageService: MessageService) {}
 }
